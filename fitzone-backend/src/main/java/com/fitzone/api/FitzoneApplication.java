e4e26de765182a8f164023b1045eb4e9c7f4ecf4
Yasith Inosh <yasithinosh1@gmail.com>
2026-07-01 20:30:58 +0530

    develop backend install mvn and check all dependencies

---
+++ fitzone-backend/src/main/java/com/fitzone/api/FitzoneApplication.java
@@ -0,0 +1,13 @@
+package com.fitzone.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitzoneApplication.class, args);
+	}
+
+}
---
+++ fitzone-backend/src/test/java/com/fitzone/api/FitzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitzone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
